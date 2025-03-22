5eff4ba772f70ca1e1346589557b9d407ac57023
therock ani <anitherock44@gmail.com>
2025-03-23 00:43:44 +0100

    final commit

--- contracts/Metacircle.sol
+++ contracts/Metacircle.sol
@@ -40,7 +40,7 @@ contract Metacircle {
     // User Registration
     function createUser(string memory _username, string memory _bio) public {
         require(bytes(users[msg.sender].username).length == 0, "User already exists.");
-        users[msg.sender] = User(msg.sender, _username, _bio); 
+        users[msg.sender] = User(msg.sender, _username, _bio);
     }
 
     // Function to get the username from an account address
@@ -58,5 +58,69 @@ contract Metacircle {
         newPost.author = msg.sender;
         newPost.content = _content;
         newPost.likes = 0;
+    }
+
+    // Liking a post
+    function likePost(uint _postId) public {
+        require(bytes(users[msg.sender].username).length != 0, "User does not exist.");
+        require(posts[_postId].author != address(0), "Post does not exist.");
+        require(!posts[_postId].likedBy[msg.sender], "Already liked this post.");
+        posts[_postId].likes++; 
+        posts[_postId].likedBy[msg.sender] = true;
+    }
+
+    // Unliking a post
+    function unlikePost(uint _postId) public {
+        require(bytes(users[msg.sender].username).length != 0, "User does not exist.");
+        require(posts[_postId].author != address(0), "Post does not exist."); 
+        require(posts[_postId].likedBy[msg.sender], "You haven't liked this post.");
+        posts[_postId].likes--;
+        posts[_postId].likedBy[msg.sender] = false;
+    }
+
+    // Commenting on a post
+    function commentOnPost(uint _postId, string memory _content) public {
+        require(bytes(users[msg.sender].username).length != 0, "User does not exist.");
+        require(posts[_postId].author != address(0), "Post does not exist.");
+        commentCount++;
+        Comment storage newComment = comments[commentCount];
+        newComment.commentId = commentCount;
+        newComment.authorUsername = users[msg.sender].username;  // Store the username of the commenter
+        newComment.postId = _postId;
+        newComment.content = _content; 
+        
+        // Add comment ID to the post's commentIds array
+        posts[_postId].commentIds.push(commentCount);
+    }
+
+    // Check if a post is liked by a user
+    function isPostLikedByUser(uint _postId, address _user) public view returns (bool) {
+        require(posts[_postId].author != address(0), "Post does not exist.");
+        return posts[_postId].likedBy[_user]; 
+    }
+
+    // Check if a user is registered
+    function isUserRegistered(address _user) public view returns (bool) {
+        return bytes(users[_user].username).length != 0;
+    }
+
+    // Get the username of the post author
+    function getPostAuthorUsername(uint _postId) public view returns (string memory) {
+        require(posts[_postId].author != address(0), "Post does not exist.");
+        address authorAddress = posts[_postId].author;
+        return users[authorAddress].username;
+    }
+
+    // Get all comments for a post
+    function getCommentsForPost(uint _postId) public view returns (Comment[] memory) {
+        require(posts[_postId].author != address(0), "Post does not exist.");
+        uint commentCountForPost = posts[_postId].commentIds.length;
+        Comment[] memory postComments = new Comment[](commentCountForPost);
+        
+        for (uint i = 0; i < commentCountForPost; i++) {
+            postComments[i] = comments[posts[_postId].commentIds[i]];
+        }
+        
+        return postComments;
     }
 }
