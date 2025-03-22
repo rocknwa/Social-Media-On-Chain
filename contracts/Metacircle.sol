5f2a275da592e506b32c726863e2bd3e7fd4295d
therock ani <anitherock44@gmail.com>
2025-03-23 00:29:26 +0100

    additional commit

--- contracts/Metacircle.sol
+++ contracts/Metacircle.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.0;
 
 contract Metacircle {
 
-    // Struct to represent a User
+    // Struct to represent a user
     struct User {
         address userAddress;
         string username;
         string bio;
     }
     
-    // Struct to represent a Post
+    // Struct to represent a post
     struct Post {
         uint postId;
         address author;
@@ -20,10 +20,10 @@ contract Metacircle {
         uint[] commentIds;  // Array to store comment IDs associated with the post
     }
 
-    // Struct to represent a Comment
+    // Struct to represent a comment
     struct Comment {
         uint commentId;
-        string authorUsername; // Store the username of the commenter
+        string authorUsername;  // Store the username of the commenter
         uint postId;
         string content;
     }
@@ -37,4 +37,26 @@ contract Metacircle {
     uint public postCount;
     uint public commentCount;
 
+    // User Registration
+    function createUser(string memory _username, string memory _bio) public {
+        require(bytes(users[msg.sender].username).length == 0, "User already exists.");
+        users[msg.sender] = User(msg.sender, _username, _bio); 
+    }
+
+    // Function to get the username from an account address
+    function getUsername(address _userAddress) public view returns (string memory) {
+        require(bytes(users[_userAddress].username).length != 0, "User does not exist.");
+        return users[_userAddress].username;
+    }
+
+    // Creating a post
+    function createPost(string memory _content) public {
+        require(bytes(users[msg.sender].username).length != 0, "User does not exist.");
+        postCount++;
+        Post storage newPost = posts[postCount];
+        newPost.postId = postCount;
+        newPost.author = msg.sender;
+        newPost.content = _content;
+        newPost.likes = 0;
+    }
 }
